4bfae820dde05452bd34ea8b2537f4bb58aa5a18
Azeem Sajjad <azeemsajjad@Azeems-MacBook-Pro.local>
2024-02-09 19:00:28 +0530

    ok

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.get("/api/get-country", async (req, res) => {
   try {
     const response = await axios.get("https://api.first.org/data/v1/countries");
-    res.json({ success: true, result: response.data.data });
+    res.json({ success: true, result: Object.values(response.data.data) });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
