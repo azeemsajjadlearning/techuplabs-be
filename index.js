5648b0a195e16188d74f8ec3544018b3c8e98d7e
Azeem Sajjad <azeemsajjad@Azeems-MacBook-Pro.local>
2024-02-09 18:56:40 +0530

    ok

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/api/get-country", async (req, res) => {
   try {
